4fa138452df3885a1dcd90ee7e024293e197e7b1
Ata KALDIRAK <atakldrkpersonal@gmail.com>
2025-01-24 12:03:08 +0300

    Project added to github

---
+++ Rocket Static Test/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace Rocket_Static_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            groupBox6 = new GroupBox();
+            groupBox4 = new GroupBox();
+            groupBox3 = new GroupBox();
+            button2 = new Button();
+            button1 = new Button();
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            groupBox2 = new GroupBox();
+            button5 = new Button();
+            button6 = new Button();
+            label3 = new Label();
+            comboBox3 = new ComboBox();
+            groupBox7 = new GroupBox();
+            groupBox5 = new GroupBox();
+            label2 = new Label();
+            label4 = new Label();
+            button3 = new Button();
+            comboBox2 = new ComboBox();
+            button4 = new Button();
+            label5 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox6.SuspendLayout();
+            groupBox7.SuspendLayout();
+            groupBox5.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.LightSlateGray;
+            groupBox1.Controls.Add(groupBox5);
+            groupBox1.Controls.Add(groupBox7);
+            groupBox1.Controls.Add(groupBox4);
+            groupBox1.Controls.Add(groupBox2);
+            groupBox1.Font = new Font("Britannic Bold", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            groupBox1.ForeColor = SystemColors.ControlLightLight;
+            groupBox1.Location = new Point(952, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(300, 657);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "    Zagora Static Test Control";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // groupBox6
+            // 
+            groupBox6.BackColor = Color.White;
+            groupBox6.Controls.Add(button5);
+            groupBox6.Controls.Add(comboBox3);
+            groupBox6.Controls.Add(button6);
+            groupBox6.Controls.Add(label3);
+            groupBox6.Location = new Point(526, 493);
+            groupBox6.Name = "groupBox6";
+            groupBox6.Size = new Size(168, 74);
+            groupBox6.TabIndex = 4;
+            groupBox6.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            groupBox4.BackColor = Color.White;
+            groupBox4.Location = new Point(6, 158);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(288, 341);
+            groupBox4.TabIndex = 3;
+            groupBox4.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            groupBox3.BackColor = Color.White;
+            groupBox3.Location = new Point(526, 421);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(168, 70);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Britannic Bold", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(6, 86);
+            button2.Name = "button2";
+            button2.Size = new Size(129, 22);
+            button2.TabIndex = 3;
+            button2.Text = "DISCONNECT";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Britannic Bold", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(6, 58);
+            button1.Name = "button1";
+            button1.Size = new Size(129, 22);
+            button1.TabIndex = 1;
+            button1.Text = "CONNECT";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Britannic Bold", 10F);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(34, 10);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 16);
+            label1.TabIndex = 2;
+            label1.Text = "COM PORT";
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Britannic Bold", 10F);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(6, 29);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(129, 23);
+            comboBox1.TabIndex = 1;
+            comboBox1.Text = "COM13";
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = Color.White;
+            groupBox2.Location = new Point(6, 29);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(288, 123);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Britannic Bold", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button5.ForeColor = SystemColors.ActiveCaptionText;
+            button5.Location = new Point(87, 44);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 22);
+            button5.TabIndex = 11;
+            button5.Text = "DISC";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Britannic Bold", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button6.ForeColor = SystemColors.ActiveCaptionText;
+            button6.Location = new Point(6, 44);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 22);
+            button6.TabIndex = 8;
+            button6.Text = "CONNECT";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Britannic Bold", 8F);
+            label3.ForeColor = SystemColors.ActiveCaptionText;
+            label3.Location = new Point(55, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(45, 12);
+            label3.TabIndex = 10;
+            label3.Text = "CAMERA";
+            // 
+            // comboBox3
+            // 
+            comboBox3.Font = new Font("Britannic Bold", 10F);
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(6, 15);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(156, 23);
+            comboBox3.TabIndex = 9;
+            comboBox3.Text = "CHOOSE CAMERA";
+            // 
+            // groupBox7
+            // 
+            groupBox7.BackColor = Color.White;
+            groupBox7.Controls.Add(label4);
+            groupBox7.Controls.Add(label5);
+            groupBox7.Controls.Add(button3);
+            groupBox7.Controls.Add(button4);
+            groupBox7.Controls.Add(comboBox2);
+            groupBox7.Location = new Point(153, 505);
+            groupBox7.Name = "groupBox7";
+            groupBox7.Size = new Size(141, 146);
+            groupBox7.TabIndex = 13;
+            groupBox7.TabStop = false;
+            // 
+            // groupBox5
+            // 
+            groupBox5.BackColor = Color.White;
+            groupBox5.Controls.Add(label2);
+            groupBox5.Controls.Add(button2);
+            groupBox5.Controls.Add(comboBox1);
+            groupBox5.Controls.Add(button1);
+            groupBox5.Controls.Add(label1);
+            groupBox5.Location = new Point(6, 505);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Size = new Size(141, 146);
+            groupBox5.TabIndex = 14;
+            groupBox5.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Britannic Bold", 11F);
+            label2.ForeColor = Color.Red;
+            label2.Location = new Point(15, 127);
+            label2.Name = "label2";
+            label2.Size = new Size(110, 16);
+            label2.TabIndex = 4;
+            label2.Text = "DISCONNECTED";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Britannic Bold", 11F);
+            label4.ForeColor = Color.Red;
+            label4.Location = new Point(16, 127);
+            label4.Name = "label4";
+            label4.Size = new Size(110, 16);
+            label4.TabIndex = 9;
+            label4.Text = "DISCONNECTED";
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Britannic Bold", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.ForeColor = SystemColors.ActiveCaptionText;
+            button3.Location = new Point(7, 86);
+            button3.Name = "button3";
+            button3.Size = new Size(129, 22);
+            button3.TabIndex = 8;
+            button3.Text = "DISCONNECT";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            comboBox2.Font = new Font("Britannic Bold", 10F);
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(7, 29);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(129, 23);
+            comboBox2.TabIndex = 5;
+            comboBox2.Text = "CHOOSE CAMERA";
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Britannic Bold", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.ForeColor = SystemColors.ActiveCaptionText;
+            button4.Location = new Point(7, 58);
+            button4.Name = "button4";
+            button4.Size = new Size(129, 22);
+            button4.TabIndex = 6;
+            button4.Text = "CONNECT";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Britannic Bold", 10F);
+            label5.ForeColor = SystemColors.ActiveCaptionText;
+            label5.Location = new Point(39, 10);
+            label5.Name = "label5";
+            label5.Size = new Size(57, 16);
+            label5.TabIndex = 7;
+            label5.Text = "CAMERA";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1264, 681);
+            Controls.Add(groupBox6);
+            Controls.Add(groupBox1);
+            Controls.Add(groupBox3);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox6.ResumeLayout(false);
+            groupBox6.PerformLayout();
+            groupBox7.ResumeLayout(false);
+            groupBox7.PerformLayout();
+            groupBox5.ResumeLayout(false);
+            groupBox5.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox4;
+        private GroupBox groupBox3;
+        private Label label1;
+        private ComboBox comboBox1;
+        private Button button2;
+        private Button button1;
+        private GroupBox groupBox6;
+        private Button button5;
+        private ComboBox comboBox3;
+        private Button button6;
+        private Label label3;
+        private GroupBox groupBox5;
+        private GroupBox groupBox7;
+        private Label label2;
+        private Label label4;
+        private Label label5;
+        private Button button3;
+        private Button button4;
+        private ComboBox comboBox2;
+    }
+}
